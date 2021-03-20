c4cb59b550545e83a2ed740e52c9137ef18ee8b1
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-03-20 13:00:58 -0500

    connect to jawsdb

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,30 @@ const sequelize = require('../config/connection');
 const { truncate } = require('./User');
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+    static upvote(body, models) {
+      return models.Vote.create({
+        user_id: body.user_id,
+        post_id: body.post_id
+      }).then(() => {
+        return Post.findOne({
+          where: {
+            id: body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        });
+      });
+    }
+  }
 
 Post.init(
     {
@@ -11,7 +34,7 @@ Post.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
         title: {
             type: DataTypes.STRING,
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,23 +1,35 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
+const sequelize = require('../../config/connection');
+
 
 // get all users
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [['created_at', 'DESC']], 
-        include: [
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+   })
 });
 
 router.get('/:id', (req, res) => {
@@ -25,7 +37,8 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at',   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       include: [
         {
           model: User,
@@ -60,6 +73,17 @@ router.post('/', (req, res) => {
       });
 });
   
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
+
 router.put('/:id', (req, res) => {
     Post.update(
       {
